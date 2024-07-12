4ebbe0b028910b9af48bcd797e92e2fa7abcd743
Selin Toker <st798@cornell.edu>
2024-07-13 02:00:04 +0300

    Add Unit Testing & API

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import BookingPage from "./screens/BookingPage";
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+const availableTimes = ["17:00", "17:30", "18:00"];
+test("renders the BookingForm heading", () => {
+  render(<BookingPage availableTimes={availableTimes} />);
+  const headingElement = screen.getByText("Booking Page");
+  expect(headingElement).toBeInTheDocument();
 });
